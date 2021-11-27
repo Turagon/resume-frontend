aadab06dec95a56f1500043bcaa1f92b774e8376
rexlin <rexlin6245@gmail.com>
2021-11-27 15:56:13 -0800

    change frontend request methid

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -22,7 +22,7 @@ export default class Login extends Component {
       return this.setState({ passwordWarning: true })
     }
 
-    axios.post('https://turagon.github.io/resume-frontend/', {name, password})
+    axios.post('https://rex-resume.herokuapp.com/', {name, password})
     .then(response => {
       if (response.data.status === 'error') {
         store.dispatch({ type: 'editError', data: response.data.message })
--- src/setProxy.js
+++ src/setProxy.js
@@ -1,11 +1,11 @@
-const proxy = require('http-proxy-middleware')
+// const proxy = require('http-proxy-middleware')
 
-module.exports = function (app) {
-  app.use(
-    proxy('https://turagon.github.io/resume-frontend/', {
-      target: 'https://rex-resume.herokuapp.com/',
-      changeOrigin: true,
-      // pathRewrite: { '^/': '/' }
-    })
-  )
-}
+// module.exports = function (app) {
+//   app.use(
+//     proxy('https://turagon.github.io/resume-frontend/', {
+//       target: 'https://rex-resume.herokuapp.com/',
+//       changeOrigin: true,
+//       // pathRewrite: { '^/': '/' }
+//     })
+//   )
+// }
