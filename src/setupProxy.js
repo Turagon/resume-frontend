6b249607c176f8dd22d93723e266f69d09cd5118
rexlin <rexlin6245@gmail.com>
2021-11-28 00:25:51 -0800

    modify links

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,17 +2,17 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
-    proxy('https://turagon.github.io', {
+    proxy('/', {
       target: 'https://rex-resume.herokuapp.com/',
       changeOrigin: true,
     }),
-    proxy('https://turagon.github.io/user', {
-      target: 'https://rex-resume.herokuapp.com/user',
-      changeOrigin: true,
-    }),
-    proxy('https://turagon.github.io/admin', {
-      target: 'https://rex-resume.herokuapp.com/admin',
-      changeOrigin: true,
-    })
+    // proxy('https://turagon.github.io/user', {
+    //   target: 'https://rex-resume.herokuapp.com/user',
+    //   changeOrigin: true,
+    // }),
+    // proxy('https://turagon.github.io/admin', {
+    //   target: 'https://rex-resume.herokuapp.com/admin',
+    //   changeOrigin: true,
+    // })
   )
 }
